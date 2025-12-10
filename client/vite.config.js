3af372a82af0ed9fe936e580b4a7d4274ee05551
worriedlemon <emilshteinberg+imprt@yandex.ru>
2025-12-10 19:36:19 +0300

    Добавил проксирование /uploads в конфиг для работы с изображениями

--- client/vite.config.js
+++ client/vite.config.js
@@ -10,6 +10,10 @@ export default defineConfig({
       '/api': {
         target: 'http://localhost:5000', // Ваш Express сервер
         changeOrigin: true
+      },
+      '/uploads': {
+        target: 'http://localhost:5000',
+        changeOrigin: true
       }
     }
   },
